fe58ec009725358f528a6675e4180c7dd13092c6
TriadSemi <Lbeno@triadsemi.com>
2018-01-25 11:20:41 -0600

    Update ts4231_config_example.ino

--- examples/ts4231_config_example/ts4231_config_example.ino
+++ examples/ts4231_config_example/ts4231_config_example.ino
@@ -1,33 +1,6 @@
 /*******************************************************************
     Copyright (C) 2017 Triad Semiconductor
-    All rights reserved.
-    Redistribution and use in source and binary forms, with or without
-    modification, are permitted provided that the following conditions
-    are met:
-    1. Redistributions of source code must retain the above copyright
-       notice, this list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright
-       notice, this list of conditions and the following disclaimer in the
-       documentation and/or other materials provided with the distribution.
-    3. Neither the name of the the copyright holder nor the names of its contributors
-       may be used to endorse or promote products derived from this software
-       without specific prior written permission.
-    4. Use of this source code or binary forms in commercial products shall require explicit
-       written consent of Triad Semiconductor
-    5. Silicon ASIC implementation of algorithims in this source is strictly prohibited, FPGA versions
-       shall be permitted provided the source is open and attribution is made to this original work.
-    THIS SOFTWARE IS PROVIDED BY THE THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND
-    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-    ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
-    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-    OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-    HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-    OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-    SUCH DAMAGE.
-
+    
     ts4231_config_example.ino - Example application for configuring the Triad
               Semiconductor TS4231 Light to Digital converter.
     Created by: John Seibel
